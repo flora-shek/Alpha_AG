12c4f49d30201c2c1ac6a834c8d0f988c8c6aee8
Shekinah Florance <84413589+flora-shek@users.noreply.github.com>
2024-12-27 07:39:01 +0000

    created navbar ,footer

--- myapp/views.py
+++ myapp/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+
+
 
 def index(request):
-    return HttpResponse("<h1>Welcome to home page</h1>")
+    return render(request,'layout.html')
